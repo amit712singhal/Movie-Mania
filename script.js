7e388c75f04a425ec40e9da84de7dc5612cb7eb1
Amit Singhal <rakshit.singhal712@gmail.com>
2024-11-21 16:58:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "YOUR_API_KEY";
+const API_KEY = "0e4a5f9c3b67e4da0c08f721ad5ec173";
 const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`;
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query="`;
